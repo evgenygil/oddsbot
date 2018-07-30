492e2794aee3331c2e2e1ce575cab32ad9b30cb7
Gil <gil707@gmail.com>
2018-07-30 12:30:43 +0300

    add logging

--- consoleApp.js
+++ consoleApp.js
@@ -27,13 +27,7 @@ db.on('error', function (err) {
     // let oldlinks = await [];
     await console.log(moment().format('DD.MM.YYYY HH:mm') + ': Start working...');
 
-    await Match.update({date: {$lt: moment().format('DD.MM.YYYY HH:mm')}}, {archive: true}, function (err, tank) {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log(tank)
-        }
-    });
+    await Match.update({date: {$lt: moment().format('DD.MM.YYYY HH:mm')}}, {archive: true}, {multi: true});
 
     let matches = await parser.parseMatches().catch((e) => logger.error('parseMatches error: ', e.stack));
 
